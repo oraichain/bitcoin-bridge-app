a556536fa6ad3d4b97122de6e7517811cddb0474
Pham Tu <tubackkhoa@gmail.com>
2023-12-08 17:56:42 +0700

    add redirect

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 export default function Custom404() {
+  const router = useRouter();
+
+  if (router.asPath !== router.route && router.route === '/') {
+    router.push(router.asPath);
+  }
   return (
     <div className="min-w-128 flex flex-col gap-6 h-full w-full justify-center items-center">
       <div className="-mt-8">
