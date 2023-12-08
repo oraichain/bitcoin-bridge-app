f9e5f560e593eb2637dd38506b413d70f8fd07ad
Pham Tu <tubackkhoa@gmail.com>
2023-12-08 17:53:48 +0700

    add firebase

---
+++ apps/app/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function Custom404() {
+  return (
+    <div className="min-w-128 flex flex-col gap-6 h-full w-full justify-center items-center">
+      <div className="-mt-8">
+        <h1>Welcome to OraiBtc Subnet dApp!</h1>
+      </div>
+    </div>
+  );
+}
